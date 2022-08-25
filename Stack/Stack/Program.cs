cb5036e4aa2e724f7a7507edee4420ad5cdcab19
“Love <opera.prem@gmail.com>
2022-08-25 20:11:49 +0430

    Added:StackDataStuructr

---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+namespace Stack1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Stack myStack = new Stack();//FIFO
+            myStack.Push("Hello");
+            myStack.Push(2);
+            myStack.Push("True");
+            myStack.Push(4.5);
+            myStack.Push(null);
+            myStack.Push("Ends of Stack");
+
+            Console.WriteLine($"The length of the Stack is - {myStack.Count}");
+            Console.WriteLine($"myStack has a 3 => {myStack.Contains(3)}");
+
+
+
+
+            Console.WriteLine($"The peak of the Stack is - {myStack.Pop()}");
+            myStack.Pop();
+            foreach(var item in myStack)
+            {
+                Console.WriteLine(item);
+
+            }
+
+                
+        }
+    }
+}
